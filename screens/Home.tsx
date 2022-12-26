4959b08e033ce5cd7512b3f05965c1344e52c5f1
dyegolara <dyegolara@icloud.com>
2022-12-25 21:48:46 -0600

    add nativewind

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,31 +1,13 @@
-import { StyleSheet } from 'react-native';
+import {  View } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text } from '../components/Themed';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Home Ewe</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text >Home Ewe</Text>
       <EditScreenInfo />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
